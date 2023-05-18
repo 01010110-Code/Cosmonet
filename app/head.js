eb4f381b703dbbb16d0c817ba59f35e030d6fc6b
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-05-18 03:34:50 +0100

    added an import to the head file

--- app/head.js
+++ app/head.js
@@ -1,9 +1,11 @@
+import Head from "next/head";
+
 const Head = () => (
-  <>
+  <Head>
     <title>Cosmonet</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.png" />
-  </>
+  </Head>
 );
 
 export default Head;
