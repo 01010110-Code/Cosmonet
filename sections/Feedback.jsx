6b736f2225e1d074634b9b836435d664fe6eafdd
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-05-18 02:46:26 +0100

    feedback section component

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -29,9 +29,11 @@ const Feedback = () => (
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          “With the development of today's technology, metaverse is very useful
-          for today's work, or can be called web 3.0. by using metaverse you can
-          use it as anything”
+          "Given the advancements in technology, the metaverse has emerged as a
+          valuable tool for modern-day work, often referred to as Web 3.0.
+          Leveraging the metaverse empowers individuals to explore boundless
+          possibilities and redefine their digital experiences, seamlessly
+          integrating it into various facets of their lives."
         </p>
       </motion.div>
 
